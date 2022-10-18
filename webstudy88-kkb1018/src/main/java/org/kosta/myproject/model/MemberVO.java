8c13ff34acf95d4c999fd4070c6e0df0a4528560
zxcaxaz <gam98846@gmail.com>
2022-10-18 16:55:21 +0900

    iu member branch 에서 MemberVO id, register

--- webstudy88-kkb1018/src/main/java/org/kosta/myproject/model/MemberVO.java
+++ webstudy88-kkb1018/src/main/java/org/kosta/myproject/model/MemberVO.java
@@ -4,5 +4,12 @@ import java.io.Serializable;
 
 public class MemberVO implements Serializable {
 	private static final long serialVersionUID = -6079971115987091112L;
+	private String id;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
 	
 }
